e9270262ffbcb378ff88e1ae13ff84a53d4c3c0c
Sen4ek <51993614+Sen4ek@users.noreply.github.com>
2020-07-29 19:35:30 +0700

    Init

---
+++ src/main/java/com/summer/gateway/models/ApiRequest.java
@@ -0,0 +1,53 @@
+package com.summer.gateway.models;
+
+
+import java.util.Objects;
+
+public class ApiRequest {
+
+    private final String method_type;
+    private final String path;
+    private final String api_version;
+
+    public ApiRequest(String method_type, String path, String api_version) {
+        this.method_type = method_type;
+        this.path = path;
+        this.api_version = api_version;
+    }
+
+    public String getMethod_type() {
+        return method_type;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getApi_version() {
+        return api_version;
+    }
+
+    @Override
+    public String toString() {
+        return "Api{" +
+                "method_type='" + method_type + '\'' +
+                ", path='" + path + '\'' +
+                ", api_version='" + api_version + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ApiRequest apiRequest = (ApiRequest) o;
+        return Objects.equals(method_type, apiRequest.method_type) &&
+                Objects.equals(path, apiRequest.path) &&
+                Objects.equals(api_version, apiRequest.api_version);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(method_type, path, api_version);
+    }
+}
